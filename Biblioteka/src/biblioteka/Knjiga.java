5404d9d5a99964707b28838b4aed93381cd6f40d
tanja-h <Tanja@TANJA-PC>
2017-04-03 01:01:45 +0200

    uvedena klasa autori i atribut LinkedList autori umesto stringa autori

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,9 +1,11 @@
 package biblioteka;
 
+import java.util.LinkedList;
+
 public class Knjiga {
 	private String naslov;
 	private String isbn;
-	private String autor;
+	private LinkedList<Autor> autori;
 	private String izdavac;
 	private String izdanje;
 	
@@ -13,9 +15,10 @@ public class Knjiga {
 	public String getIsbn() {
 		return isbn;
 	}
-	public String getAutor() {
-		return autor;
+	public LinkedList<Autor> getAutori() {
+		return autori;
 	}
+	
 	public String getIzdavac() {
 		return izdavac;
 	}
@@ -29,8 +32,8 @@ public class Knjiga {
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
-	public void setAutor(String autor) {
-		this.autor = autor;
+	public void setAutori(LinkedList<Autor> autori) {
+		this.autori = autori;
 	}
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
